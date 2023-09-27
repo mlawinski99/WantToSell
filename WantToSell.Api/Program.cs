1d09f546a40a17f20a5d33d1fc4214774e27bf80
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:21:32 +0200

    projects, api + docker support

---
+++ WantToSell.Api/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using WantToSell.Application;
+using WantToSell.Infrastructure;
+using WantToSell.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplicationServiceCollection();
+builder.Services.AddInfrastructureServiceCollection();
+builder.Services.AddPersistenceServiceCollection(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll",
+		builder =>
+			builder.AllowAnyOrigin()
+				.AllowAnyHeader()
+				.AllowAnyMethod());
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
