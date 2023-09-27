4e4993659f30e22da8f9e94e44f8580dc3c28307
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:19:30 +0200

    dbcontext

---
+++ WantToSell.Persistence/DbContext/WantToSellContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using WantToSell.Domain;
+using WantToSell.Domain.Shared;
+
+namespace WantToSell.Persistence.DbContext
+{
+	public class WantToSellContext : Microsoft.EntityFrameworkCore.DbContext
+	{
+		public WantToSellContext(DbContextOptions<WantToSellContext> options) : base(options)
+		{
+				
+		}
+		
+		public DbSet<Category> Categories { get; set; }
+
+		public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+		{
+			foreach (var entry in base.ChangeTracker.Entries<Entity>().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified))
+			{
+				if (entry.State == EntityState.Added)
+				{
+					entry.Entity.DateCreatedUtc = DateTime.UtcNow;
+					//@todo createdby
+				}
+
+				if (entry.State == EntityState.Modified)
+				{
+					entry.Entity.DateModifiedUtc = DateTime.UtcNow;
+				}
+			}
+
+			return base.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
