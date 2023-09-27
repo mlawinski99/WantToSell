79b82953a72c48c8ea42bd07a59c0859d0d2f966
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:18:28 +0200

    category mapper

---
+++ WantToSell.Application/Mappings/CategoryProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using WantToSell.Application.Features.Category.Models;
+using WantToSell.Domain;
+
+namespace WantToSell.Application.Mappings
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryListModel>();
+            CreateMap<CategoryCreateModel, Category>();
+            CreateMap<CategoryUpdateModel, Category>();
+        }
+    }
+}
