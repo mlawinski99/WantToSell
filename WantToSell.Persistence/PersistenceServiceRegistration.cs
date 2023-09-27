e571290a9ec633c7951588d4c1f6b822cc8d7e7e
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:21:39 +0200

    persistence services

---
+++ WantToSell.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WantToSell.Application.Contracts.DataAccess;
+using WantToSell.Persistence.DbContext;
+using WantToSell.Persistence.Repositories;
+
+namespace WantToSell.Persistence
+{
+	public static class PersistenceServiceRegistration
+	{
+			public static IServiceCollection AddPersistenceServiceCollection(
+				this IServiceCollection services, IConfiguration configuration)
+			{
+				services.AddDbContext<WantToSellContext>(options => 
+					options.UseSqlServer(configuration.GetConnectionString("WantToSellDbConnectionString")));
+
+				services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+				services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+				return services;
+			}
+	}
+}
