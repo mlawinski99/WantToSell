4976c1970fecb51884e542f039f9a191a664cf85
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:18:18 +0200

    category validators

---
+++ WantToSell.Application/Features/Category/Validators/CategoryCreateModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using WantToSell.Application.Features.Category.Models;
+
+namespace WantToSell.Application.Features.Category.Validators;
+
+public class CategoryCreateModelValidator : AbstractValidator<CategoryCreateModel>
+{
+    public CategoryCreateModelValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Name is required and can not be empty!");
+    }
+}
---
+++ WantToSell.Application/Features/Category/Validators/CategoryUpdateModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using WantToSell.Application.Features.Category.Models;
+
+namespace WantToSell.Application.Features.Category.Validators;
+
+public class CategoryUpdateModelValidator : AbstractValidator<CategoryUpdateModel>
+{
+    public CategoryUpdateModelValidator()
+    {
+        RuleFor(p => p.Id).NotNull().WithMessage("Id is required!");
+        RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Name is required and can not be empty!");
+    }
+}
