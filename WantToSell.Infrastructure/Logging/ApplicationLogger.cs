77188cb23f234d2b5e23bf5aa319986365640548
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:44:59 +0200

    logger

---
+++ WantToSell.Infrastructure/Logging/ApplicationLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WantToSell.Application.Contracts.Logs;
+
+namespace WantToSell.Infrastructure.Logs
+{
+	public class ApplicationLogger<T> : IApplicationLogger<T>
+	{
+		private readonly ILogger<T> _logger;
+
+		public ApplicationLogger(ILoggerFactory loggerFactory)
+		{
+			_logger = loggerFactory.CreateLogger<T>();
+		}
+		public void LogInformation(string message, params object[] args)
+		{
+			_logger.LogInformation(message, args);
+		}
+
+		public void LogWarning(string message, params object[] args)
+		{
+			_logger.LogWarning(message, args);
+		}
+
+		public void LogError(string message, params object[] args)
+		{
+			_logger.LogError(message, args);
+		}
+	}
+}
