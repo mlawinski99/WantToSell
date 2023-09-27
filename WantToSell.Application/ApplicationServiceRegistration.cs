71a5a9f7c953edd2fb634674e6e20eaa8929a990
Mateusz Ławinski <56017422+mlawinski99@users.noreply.github.com>
2023-09-27 17:19:18 +0200

    infrastructure and application services

---
+++ WantToSell.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WantToSell.Application
+{
+	public static class ApplicationServiceRegistration
+	{
+		public static IServiceCollection AddApplicationServiceCollection(
+			this IServiceCollection services)
+		{
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+			return services;
+		}
+	}
+}
---
+++ WantToSell.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WantToSell.Application.Contracts.Logs;
+using WantToSell.Infrastructure.Logs;
+
+namespace WantToSell.Infrastructure
+{
+	public static class InfrastructureServiceRegistration
+	{
+		public static IServiceCollection AddInfrastructureServiceCollection(
+			this IServiceCollection services)
+		{
+			services.AddScoped(typeof(IApplicationLogger<>), typeof(ApplicationLogger<>));
+
+			return services;
+		}
+	}
+}
