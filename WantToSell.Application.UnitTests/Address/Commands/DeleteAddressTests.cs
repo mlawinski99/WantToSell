84b65eb2b12a63d83d5506b66e131de814a3cc09
mlawinski99 <mlawinskiv1@gmail.com>
2023-12-30 21:33:26 +0100

    address unit tests

---
+++ WantToSell.Application.UnitTests/Address/Commands/DeleteAddressTests.cs
@@ -0,0 +1,91 @@
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using WantToSell.Application.Contracts.Identity;
+using WantToSell.Application.Contracts.Persistence;
+using WantToSell.Application.Exceptions;
+using WantToSell.Application.Features.Address.Commands;
+using WantToSell.Application.Mappings;
+using WantToSell.Application.UnitTests.Mocks;
+using WantToSell.Domain.Interfaces;
+using Xunit;
+
+namespace WantToSell.Application.UnitTests.Address.Commands;
+
+public class DeleteAddressTests
+{
+    private readonly Mock<IAddressRepository> _addressMockRepository;
+    private readonly Mock<IUserService> _userMockRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteAddressTests()
+    {
+        _addressMockRepository = MockAddressRepository.GetAddressRepositoryMock();
+        _userMockRepository = MockUserRepository.GetUserRepositoryMock();
+
+        var mapperConfiguration = new MapperConfiguration(c =>
+        {
+            c.AddProfile<AddressProfile>();
+        });
+
+        _mapper = mapperConfiguration.CreateMapper();
+    }
+    
+    [Fact]
+    public async Task DeleteAddressTest_ValidRequest_ShouldReturnTrue()
+    {
+        var existingAddressId = new Guid("6565c140-1abd-4ba8-be8f-1c0a38802913");
+        
+        var mockUserContext = new Mock<IUserContext>();
+        var userId = _userMockRepository.Object.GetUsers().Result.First().Id; //User with address
+        mockUserContext.SetupGet(u => u.UserId).Returns(Guid.Parse(userId));
+        
+        var handler = new DeleteAddress.Handler(_addressMockRepository.Object, mockUserContext.Object);
+
+        // Act
+        var result = await handler.Handle(new DeleteAddress.Command(existingAddressId), CancellationToken.None);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DeleteAddressTest_AddressNotFound_ShouldThrowNotFoundException()
+    {
+        // Arrange
+        var notExistingAddresId = new Guid("2222c140-1abd-4ba8-be8f-1c0a38802913");
+
+        var mockUserContext = new Mock<IUserContext>();
+        var userId = _userMockRepository.Object.GetUsers().Result.First().Id; //User with address
+        mockUserContext.SetupGet(u => u.UserId).Returns(Guid.Parse(userId));
+
+        var handler = new DeleteAddress.Handler(_addressMockRepository.Object, mockUserContext.Object);
+
+        // Act
+        var exception = await Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(new DeleteAddress.Command(notExistingAddresId), CancellationToken.None));
+        
+        // Assert
+        exception.Message.Should().Be("Address does not exist!");
+        exception.Should().BeOfType<NotFoundException>();
+    }
+
+    [Fact]
+    public async Task DeleteAddressTest_AccessDenied_ShouldThrowAccessDeniedException()
+    {
+        // Arrange
+        var existingAddressId = new Guid("6565c140-1abd-4ba8-be8f-1c0a38802913");
+
+        var mockUserContext = new Mock<IUserContext>();
+        var userId = _userMockRepository.Object.GetUsers().Result.Last().Id; //User without address
+        mockUserContext.SetupGet(u => u.UserId).Returns(Guid.Parse(userId));
+
+        var handler = new DeleteAddress.Handler(_addressMockRepository.Object, mockUserContext.Object);
+
+        // Act
+        var exception = await Assert.ThrowsAsync<AccessDeniedException>(() =>
+            handler.Handle(new DeleteAddress.Command(existingAddressId), CancellationToken.None));
+
+        //Assert
+        exception.Should().BeOfType<AccessDeniedException>();
+    }
+}
---
+++ WantToSell.Application.UnitTests/Mocks/MockUserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using WantToSell.Application.Contracts.Identity;
+using WantToSell.Application.Models.Identity;
+
+namespace WantToSell.Application.UnitTests.Mocks;
+
+public class MockUserRepository
+{
+    public static Mock<IUserService> GetUserRepositoryMock()
+    {
+        var userList = new List<UserModel>
+        {
+            new()
+            {//User with address
+                Id = "02f7af17-d128-41fb-977b-2dec061114c8",
+                UserName = "testuser",
+                Name = "testuser",
+                Email = "testuser@user.com",
+                Password = "testpassword"
+            },
+            new()
+            {//User with no address
+                Id = "d9247df3-37e1-4423-847b-32cdd1b080d5",
+                UserName = "testuser2",
+                Name = "testuser2",
+                Email = "testuser2@user.com",
+                Password = "testpassword"
+            }
+        };
+        
+        var userMockRepository = new Mock<IUserService>();
+
+        userMockRepository.Setup(s => s.GetUser(It.IsAny<Guid>()))
+            .ReturnsAsync((Guid id) =>
+            {
+                return userList
+                    .FirstOrDefault(s => s.Id == id.ToString());
+            });
+      
+        userMockRepository.Setup(s => s.GetUsers())
+            .ReturnsAsync( () => userList.ToList());
+
+        return userMockRepository;
+    }
+}
